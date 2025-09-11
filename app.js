b5d59478011cf2c08d1bb421c5838ecc056fb690
Abril Cisterna <abril.cisterna23@gmail.com>
2025-09-11 00:13:47 -0300

    se avanza funcion para agregar un amigo

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+let amigoSecreto = sortearAmigo();
+
+
+function agregarAmigo() {
+    let inputAmigo = document.getElementById("amigo");
+    let amigo = inputAmigo.value
+
+   if (amigo === ""){
+        alert("Por favor, ingresa un nombre.");
+        return;
+    }
+    if (listaAmigos.includes(amigo)){
+        alert(`${amigo} ya se encuentra ingresado en la lista!`)
+        return;
+    }
+ 
+       listaAmigos.push(amigo)
+}
